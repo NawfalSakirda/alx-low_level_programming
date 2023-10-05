caa1f7097feb84ddbd0ba8f128aa5a2c41dcc86a
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-05 14:02:41 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
