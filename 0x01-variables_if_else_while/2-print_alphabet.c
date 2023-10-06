689cf1b558ed2378715228c4753c74d2738af32a
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-06 10:26:59 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabets
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alphabet = "a";
+
+	while (alphabet <= "z")
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar("\n");
+
+	return (0);
+}
