c6613fd573ac2bd2775787cd5dcade39bb0edb7e
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-06 10:08:53 -0700

    hot fix task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_dgt);
 	}
-	else if (last_dgt < 6)
+	else if (last_dgt == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_dgt);
 	}
