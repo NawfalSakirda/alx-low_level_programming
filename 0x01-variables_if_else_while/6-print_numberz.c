15674afb9d6fc82e3e1607bf6edb2ba0ec8c119e
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-06 11:48:18 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: using putchar print all single digit numbers
+ *		of base 10 starting from 0
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
