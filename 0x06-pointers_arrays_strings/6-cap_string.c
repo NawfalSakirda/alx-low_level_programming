5ca2d91c2a52f89674f577695b36c7919bc16552
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-19 12:33:34 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ * lower - tests if character is lowercase
+ *
+ * @c: character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int lower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * delim - tests if character is a delimiter
+ *
+ * @c: character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int delim(char c)
+{
+	int a;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (a = 0; a <= 11; a++)
+		if (c == delimiter[a])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalize all words of a string
+ *
+ * @s: string inpt
+ *
+ * Return: uppr string
+ */
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int del = 1;
+
+	while (*s)
+	{
+		if (delim(*s))
+			del = 1;
+		else if (lower(*S) && del)
+		{
+			*s -= 32;
+			del = 0;
+		}
+		else
+			del = 0;
+		s++;
+	}
+	return (ptr);
+}
