032e0d46e1dd7e1e734ac5342d24b923ffe0db22
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-19 09:16:07 -0700

    hf3

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 /**
  * string_toupper - it changes all lowercase letters to uppercase
  *
- * @s: string
+ * @str: string
  *
  * Return: string
  */
@@ -15,7 +15,9 @@ char *string_toupper(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
+		{
 			str[i] = str[i] - 32;
+		}
 	}
 	return (str);
 }
