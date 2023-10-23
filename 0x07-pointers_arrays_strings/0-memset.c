1cdb803f5d49669d7f6fa5683f2f473c913ee84b
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-23 01:36:18 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the address of memory to fill
+ * @b: char constant byte used as filling
+ * @n: the size of the memory to be filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
