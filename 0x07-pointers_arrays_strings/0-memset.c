d6f1502b7579c0bb093a88c7d46877d3f07757d9
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-10-23 04:36:48 -0700

    hf

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,18 +2,20 @@
 
 /**
  * _memset - fills memory with a constant byte
+ *
  * @s: the address of memory to fill
  * @b: char constant byte used as filling
  * @n: the size of the memory to be filled
+ *
+ * Return: a pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
+
 	return (s);
 }
