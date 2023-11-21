5926611920fc9d61d2c816187ab96b9fbb2af90e
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-11-20 17:06:31 -0800

    new way

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,32 +11,35 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node, *neo = malloc(sizeof(listint_t));
-	unsigned int i = 0;
+	listint_t *node, *neo;
+	unsigned int i;
 
-	if (!head || !neo)
-		return (NULL);
+	node = *head;
+	neo = malloc(sizeof(listint_t));
+	if (neo == 0)
+		return (0);
 
 	neo->n = n;
-	neo->next = NULL;
-	if (!idx)
+	if (idx == 0)
 	{
 		neo->next = *head;
 		*head = neo;
 		return (neo);
 	}
-	neo = *head;
-	while (node)
+	i = 0;
+	while (*head != 0 && i < idx - 1)
 	{
-		if (i == idx - 1)
-		{
-			neo->next = node->next;
-			node->next = neo;
-			return (neo);
-		}
-		i++;
 		node = node->next;
+		i++;
 	}
-	free(neo);
-	return (NULL);
+	if (node == 0)
+	{
+		free(neo);
+		return (0);
+	}
+
+	neo->next = node->next;
+	node->next = neo;
+
+	return (neo);
 }
