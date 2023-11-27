a59d9f4fa4ba97d42eb36f58f6f874fa74671f06
Nawfal Sakirda <nawfalsakirda@gmail.com>
2023-11-27 13:51:09 -0800

    1. Under the snow hf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == 0)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IRUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
